e1a44a4a88906fc6ef995141c094261b0a1eb7e0
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-10-18 23:17:39 +0300

    fixing footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,37 +1,45 @@
 import classes from "./Footer.module.css";
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <>
-    <footer className={classes.backGroundContainer}>
-      <div className={classes.container}>
-        <div className={classes.leftPart}>
-          <h2>Flavor&apos;s</h2>
-          <p>© Flavor&apos;s Template.All Rights Reserved.</p>
-        </div>
-        <div className={classes.rightPart}>
-          <div className={classes.firstColumn}>
-            <a>Menu</a>
-            <a>About</a>
-            <a>Hours & Location</a>
-            <a>Contact</a>
-            <a>Reservations</a>
+      <footer className={classes.backGroundContainer}>
+        <div className={classes.container}>
+          <div className={classes.leftPart}>
+            <h2>Flavor&apos;s</h2>
+            <p>© Flavor&apos;s Template.All Rights Reserved.</p>
           </div>
+          <div className={classes.rightPart}>
+            <div className={classes.firstColumn}>
+              <Link to={"/Menu"}>
+                <a>Menu</a>
+              </Link>
+              <Link to={"/About-Us"}>
+                <a>About</a>
+              </Link>
+              <Link to={"/Booking"}>
+                <a>Reservations</a>
+              </Link>
+              <a>Hours & Location</a>
+              <a>Contact</a>
+            </div>
 
-          <div className={classes.secColumn}>
-            <a>Privacy Policy</a>
-            <a>Licneses</a>
-            <a>Instructions</a>
-            <a>404</a>
-            <a>Passwords</a>
-            <a>Style Guide</a>
-          </div>
-          <div className={classes.thirdColumn}>
-            <a>Facebook</a>
-            <a>Instagram</a>
-            <a>Twitter</a>
+            <div className={classes.secColumn}>
+              <a>Privacy Policy</a>
+              <a>Licneses</a>
+              <a>Instructions</a>
+              <a>404</a>
+              <a>Passwords</a>
+              <a>Style Guide</a>
+            </div>
+            <div className={classes.thirdColumn}>
+              <a>Facebook</a>
+              <a>Instagram</a>
+              <a>Twitter</a>
+            </div>
           </div>
         </div>
-      </div>
       </footer>
     </>
   );
