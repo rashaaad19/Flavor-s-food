7a6e4a8698e3d4f16d98c6516679e277bcb2c869
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-10-18 04:31:04 +0300

    deployment attemp 7

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Flavor-s-food/", // Adjust this path based on your GitHub Pages URL
   plugins: [react()],
+  base: "/Flavor-s-food/"
+
 });
